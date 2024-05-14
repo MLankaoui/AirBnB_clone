545719f665c17ed0c467945cf670f499b28da6a4
Fatima20027 <fatima.hailou@icloud.com>
2024-05-14 16:33:25 +0100

    created console.py and added it

--- console.py
+++ console.py
@@ -1,24 +1,29 @@
+#!/usr/bin/python3
 import cmd
+
 
 class HBNBCommand(cmd.Cmd):
     """
     Command interpreter for managing HBNB data.
     """
+  
     prompt = "(hbnb) "
+    
     def do_quit(self):
         """
         Quit command to exit the program.
         """
         return True
-    
+
     def do_EOF(self):
         """
         Exit the program cleanly at end of file (EOF).
         """
         return True
-    
+  
     def emptyline(self):
         return False
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
