2073d9b132178099c975c932c1b3e4ba6c36e0ba
MLankaoui <marouanelankaoui66@gmail.com>
2024-05-14 15:49:44 +0100

    fixed all bugs in filestorage

--- models/base_model.py
+++ models/base_model.py
@@ -2,11 +2,6 @@
 import uuid
 from datetime import datetime
 from models.__init__ import storage
-"""a script that creates a class base_model"""
-
-
-strptime = datetime.strptime
-
 
 class BaseModel:
     """
@@ -24,42 +19,39 @@ class BaseModel:
 
         Args:
             *args(args): arguments
-            **kwargs(dict): attrubute values
-
+            **kwargs(dict): attribute values
         """
-        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        
         if kwargs is not None:
-            for key, vl in kwargs.items():
+            for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ('updated_at', 'created_at'):
-                        vl = strptime(vl, '%Y-%m-%dT%H:%M:%S.%f')
-                    setattr(self, key, vl)
-            self.id = kwargs.get('id')
-
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
 
-        storage.new()
+        storage.new(self)
 
     def __str__(self):
         """
         Returns a string representation of the BaseModel instance.
         """
         return "[{}] ({}) {}".format(
-            __class__.__name__, self.id, self.__dict__)
+            self.__class__.__name__, self.id, self.__dict__)
 
     def to_dict(self):
         """Returns a dictionary representation of the BaseModel instance"""
-        new_dictonary = self.__dict__.copy()
-        new_dictonary['__class__'] = self.__class__.__name__
-        new_dictonary['updated_at'] = self.updated_at.isoformat()
-        new_dictonary['created_at'] = self.created_at.isoformat()
+        new_dictionary = self.__dict__.copy()
+        new_dictionary['__class__'] = self.__class__.__name__
+        new_dictionary['updated_at'] = self.updated_at.isoformat()
+        new_dictionary['created_at'] = self.created_at.isoformat()
 
-        return new_dictonary
+        return new_dictionary
 
     def save(self):
         """
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,29 +1,42 @@
-#/usr/bin/python3
-import json
 import os
-
+import json
+import datetime
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-
     def all(self):
-        key = "{}.{}".format(self.__class__.__name__, self.id)
-        FileStorage.__objects[key] = self.to_dict()
-    
+        return self.__objects
 
     def new(self, obj):
-        obj = obj[self.__class__.__name__.__objetcs]
-        FileStorage.objects = obj
-
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        with open("file.json", "a", encoding="UTF-8") as json_file:
-            json.dumps(FileStorage, json_file, indent=4)
+        jso_dict = {}
+
+        for ky in FileStorage.__objects:
+            jso_dict[ky] = FileStorage.__objects[ky].to_dict()
+
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(jso_dict, f)
+
 
 
     def reload(self):
-        if os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, "r", encoding="UTF-8") as json_file:
-                FileStorage.__objects = json.loads(json_file)
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as file:
+                for line in file:
+                    try:
+                        json_data = json.loads(line)
+                        for key, value in json_data.items():
+                            clas_nm, obj_id = key.split('.')
+                            modl = __import__('models.' + clas_nm, fromlist=[clas_nm])
+                            cls = getattr(modl, clas_nm)
+                            value["created_at"] = datetime.datetime.strptime(value["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                            value["updated_at"] = datetime.datetime.strptime(value["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                            self.__objects[key] = cls(**value)
+                    except Exception as e:
+                        print(f"Error loading object: {e}")
+
