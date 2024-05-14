9cc15b8802312c2419d46346214169214353a21f
MLankaoui <marouanelankaoui66@gmail.com>
2024-05-14 12:18:46 +0100

    uuid casted to a string

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self):
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
