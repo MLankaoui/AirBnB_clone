91fee484c3c5f78ca79ffbfad3e0c1a0b53dae37
MLankaoui <marouanelankaoui66@gmail.com>
2024-05-19 18:07:40 +0100

    reload function some changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import json
 
-
 class FileStorage:
     """FileStorage class that handles the serialization
     and deserialization of objects to and from a JSON file."""
@@ -15,57 +14,42 @@ class FileStorage:
 
     def new(self, obj):
         """Adds a new object to the __objects dictionary
-        with a key formatted as <class name>.<id>.
-"""
+        with a key formatted as <class name>.<id>."""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
         """Serializes the __objects dictionary to
         the JSON file specified by __file_path."""
-        jso_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        json_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
 
         with open(self.__file_path, 'w') as f:
-            json.dump(jso_dict, f)
+            json.dump(json_dict, f)
 
     def reload(self):
-        """
-        deserializes the JSON file to __objects, if the JSON
-        file exists, otherwise nothing happens)
-        """
+        """Deserializes the JSON file to __objects, if the JSON
+        file exists, otherwise nothing happens."""
         try:
             with open(self.__file_path, 'r') as f:
-                dictionary_objj = json.load(f)
-
-                for key, value in dictionary_objj.items():
-                    clss_nm = value["__class__"]
-
-                    if clss_nm == 'BaseModel':
-                        from models.base_model import BaseModel
-                        obj_class = BaseModel
-                    elif clss_nm == 'User':
-                        from models.user import User
-                        obj_class = User
-                    elif clss_nm == 'Place':
-                        from models.place import Place
-                        obj_class = Place
-                    elif clss_nm == 'State':
-                        from models.state import State
-                        obj_class = State
-                    elif clss_nm == 'City':
-                        from models.city import City
-                        obj_class = City
-                    elif clss_nm == 'Amenity':
-                        from models.amenity import Amenity
-                        obj_class = Amenity
-                    elif clss_nm == 'Review':
-                        from models.review import Review
-                        obj_class = Review
-                    else:
-                        raise ImportError(
-                            f"Class {clss_nm} is not recognized")
-
+                dictionary_obj = json.load(f)
+                for key, value in dictionary_obj.items():
+                    class_name = value["__class__"]
+                    module_name = f"models.{self._to_snake_case(class_name)}"
+                    module = __import__(module_name, fromlist=[class_name])
+                    obj_class = getattr(module, class_name)
                     obj = obj_class(**value)
                     self.__objects[key] = obj
         except FileNotFoundError:
             pass
+        except KeyError as e:
+            print(f"KeyError: {e}")
+        except ImportError as e:
+            print(f"ImportError: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+
+    def _to_snake_case(self, name):
+        """Converts CamelCase to snake_case."""
+        import re
+        s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
